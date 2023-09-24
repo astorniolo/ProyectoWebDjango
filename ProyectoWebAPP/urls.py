16ac3a2ae0d82892d0c4a4710426b01a33201479
astorniolo <astorniolo@gmail.com>
2023-09-24 11:30:35 -0300

    INICIO proyecto web modif urls,views ysettings

---
+++ ProyectoWebAPP/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from ProyectoWebAPP.views import *
+
+urlpatterns = [
+    path('', home, name='Home'),   
+    path('servicios', servicios ,name='Servicios'), 
+    path('tienda', tienda ,name='Tienda'), 
+    path('blog', blog ,name='Blog'), 
+    path('contacto', contacto ,name='Contacto'), 
+]
